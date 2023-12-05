a414eb60075fe62e063403f1e9658baee93179d9
Jimmy Bondu <jimmy.bondu@zoho.com>
2023-12-05 11:32:16 +0100

    feat: try to complete day 3.2

--- challenges/day_3.py
+++ challenges/day_3.py
@@ -21,7 +21,7 @@ def find_expression_positions(
 def get_neighbors(line: str, col_start: int, col_end, n_cols: int) -> str:
     if col_start == 0:
         return "." + line[col_start : col_end + 1]
-    elif col_end == n_cols - 1:
+    elif col_end == n_cols:
         return line[col_start - 1 : col_end] + "."
     return line[col_start - 1 : col_end + 1]
 
@@ -56,9 +56,31 @@ def get_neighborhood(
     return previous_line + line + next_line
 
 
-def is_part_neighborhood(neighborhood: str) -> bool:
-    pattern = re.compile("([^0-9.])")
-    return pattern.search(neighborhood) is not None
+def is_part_neighborhood(
+    neighborhood: str,
+    expression: str,
+    neighbor_needed=1,
+) -> bool:
+    pattern = re.compile(expression)
+    last_index = 0
+    neighbor_found = 0
+
+    while (
+        pattern.search(neighborhood, last_index) is not None
+        and neighbor_found < neighbor_needed
+    ):
+        last_index = pattern.search(neighborhood, last_index).span()[1]
+        neighbor_found += 1
+
+    return neighbor_found >= neighbor_needed
+
+
+def are_neighbors(n_1: tuple[int, int, int], n_2: tuple[int, int, int]):
+    if (n_1[0] - 1 <= n_2[0] and n_2[0] <= n_1[0] + 1) and (
+        n_1[1] <= n_2[2] and n_2[1] <= n_1[2]
+    ):
+        return True
+    return False
 
 
 if __name__ == "__main__":
@@ -70,6 +92,9 @@ if __name__ == "__main__":
 
     result_1 = 0
     result_2 = 0
+
+    potential_gear_numbers = []
+    gear_products = []
 
     for i in range(n_lines):
         number_positions = find_expression_positions(lines[i], "(\d+)")
@@ -85,8 +110,37 @@ if __name__ == "__main__":
                 n_cols,
             )
 
-            if is_part_neighborhood(neighborhood):
+            if is_part_neighborhood(neighborhood, "([^0-9.])"):
                 result_1 += int(lines[i][col_start:col_end])
+
+                if is_part_neighborhood(neighborhood, "(\*)"):
+                    potential_gear_numbers.append((i, col_start, col_end))
+
+    for j in range(n_lines):
+        gear_positions = find_expression_positions(lines[j], "(\*)")
+
+        for gear_position in gear_positions:
+            col_start, col_end = gear_position
+            neighborhood = get_neighborhood(
+                j,
+                col_start,
+                col_end,
+                lines,
+                n_lines,
+                n_cols,
+            )
+
+            if is_part_neighborhood(neighborhood, "(\d+)", 2):
+                gear_products.append((j, col_start, col_end))
+
+    for gear in gear_products:
+        product = 1
+
+        for number in potential_gear_numbers:
+            if are_neighbors(gear, number) is True:
+                product *= int(lines[number[0]][number[1] : number[2]])
+
+        result_2 += product
 
     print(result_1)
     print(result_2)
