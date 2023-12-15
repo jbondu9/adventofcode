61f782310939b89eefc3511f5ea58eb17ebed4ea
Jimmy Bondu <jimmy.bondu@zoho.com>
2023-12-15 18:31:01 +0100

    feat: try to complete day 8.2

--- challenges/day_8.py
+++ challenges/day_8.py
@@ -18,6 +18,12 @@ def get_next_node(label: str, instruction: str, network: list[str]) -> str:
     return ""
 
 
+def get_all_nodes(ends_with: str, network: str) -> list[str]:
+    pattern = re.compile("([A-Z1-9]{2}" + f"{ends_with}" + "{1} =)")
+    nodes = pattern.findall(network)
+    return [n[:3] for n in nodes]
+
+
 if __name__ == "__main__":
     filename = "inputs/day_8.txt"
     data = read_file(filename)
@@ -35,3 +41,17 @@ if __name__ == "__main__":
         result_1 += 1
 
     print(result_1)
+
+    i = 0
+    starting_nodes = get_all_nodes("A", "\n".join(data[2:]))
+    ending_nodes = get_all_nodes("Z", "\n".join(data[2:]))
+    result_2 = 0
+
+    while set(starting_nodes).issubset(ending_nodes) is not True:
+        starting_nodes = [
+            get_next_node(n, instruction_sequence[i], data[2:]) for n in starting_nodes
+        ]
+        i = (i + 1) % n_instructions
+        result_2 += 1
+
+    print(result_2)
